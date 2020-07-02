2734e5b5e35a2837c4f413e2adbdacd2ea6c3815
Borah <manasjyoti.borah@sap.com>
2020-07-02 10:21:03 +0530

    retail login added

--- retailer/urls.py
+++ retailer/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import home
+from .views import home, loginPage, logoutPage, registerPage
 
 app_name = 'retailer'
 
 urlpatterns = [
     path('retailer/', home, name="home"),
+    path('login/', loginPage, name="login"),
+    path('logout/', logoutPage, name="logout"),
+    path('register/', registerPage, name="register")
 ]
--- retailer/views.py
+++ retailer/views.py
@@ -1,7 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import CreateRetailUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
+@login_required(login_url='/login')
 def home(request):
-    return render(request, "retail_home.html")
+    context = {}
+    return render(request, "retail_home.html", context)
+
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('/retailer')
+        else:
+            messages.info(request, 'Username or password is incorrect')
+    context = {}
+    return render(request, "retail_login.html", context)
+
+
+def logoutPage(request):
+    logout(request)
+    return redirect('/login')
+
+
+def registerPage(request):
+    form = CreateRetailUserForm()
+
+    if request.method == 'POST':
+        form = CreateRetailUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account is created for ' + user)
+            return redirect('/login')
+
+    context = {'form': form}
+    return render(request, "retail_register.html", context)
