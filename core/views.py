42992ed39e4376a139d4356a9d1b423cbcdd6ece
Borah <manasjyoti.borah@sap.com>
2020-07-10 20:07:30 +0530

    correction to customer registration

--- core/views.py
+++ core/views.py
@@ -5,6 +5,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.views.generic import ListView, DetailView, View
 from django.utils import timezone
 from django.contrib import messages
+from django.contrib.auth.models import Group
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
